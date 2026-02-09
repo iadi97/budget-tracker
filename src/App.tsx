39ba9bc65b989aa620b8d7554bf971bcd73329e2
Aditya Pampana <52345840+iadi97@users.noreply.github.com>
2026-02-09 17:52:09 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabase";
 import { Session } from "@supabase/supabase-js";
+
 import Auth from "./auth/Auth";
-import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -20,29 +21,20 @@ function App() {
       setSession(session);
     });
 
-    return () => subscription.unsubscribe();
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
-  if (loading) return null;
-
-  return session ? (
-    <ProtectedRoute session={session}>
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-semibold mb-4">
-          Logged in successfully
-        </h1>
-
-        <button
-          onClick={() => supabase.auth.signOut()}
-          className="bg-gray-200 px-4 py-2 rounded"
-        >
-          Logout
-        </button>
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Loading...
       </div>
-    </ProtectedRoute>
-  ) : (
-    <Auth />
-  );
+    );
+  }
+
+  return session ? <Dashboard /> : <Auth />;
 }
 
 export default App;
