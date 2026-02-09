aa8ea6bab7ce1a8c5fbb3633dabbd4f6e88380c7
Aditya Pampana <52345840+iadi97@users.noreply.github.com>
2026-02-09 17:32:30 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#C4B5FD",
+        primaryLight: "#F5F3FF"
+      }
+    }
+  },
+  plugins: []
+};
